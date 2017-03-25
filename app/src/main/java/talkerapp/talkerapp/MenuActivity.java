e40d5a25a30c8a4cfc4a419151cad7779a52d6c3
Adrian Cieniuch <adrian.cieniuch@gmail.com>
2017-03-25 14:38:18 +0100

    Zmieniłem kolorystykę, dodałem przykładowe opcje w menu

--- app/src/main/java/talkerapp/talkerapp/MenuActivity.java
+++ app/src/main/java/talkerapp/talkerapp/MenuActivity.java
@@ -80,17 +80,13 @@ public class MenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_friends) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
