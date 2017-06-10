14c3da3db3673b0bdff9120d7f28d5df7baaa3ce
Tomek Demczuk <tomekkomek@gmail.com>
2017-06-10 21:43:41 +0200

    dodane pobieranie listy userów

--- app/src/main/java/talkerapp/talkerapp/SearchFriendsActivity.java
+++ app/src/main/java/talkerapp/talkerapp/SearchFriendsActivity.java
@@ -8,6 +8,16 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tomek.UserLogged;
+import tomek.UserRegistered;
+import tomek.WSocket;
+
 public class SearchFriendsActivity extends AppCompatActivity
 {
     private RecyclerView recyclerView;
@@ -34,5 +44,30 @@ public class SearchFriendsActivity extends AppCompatActivity
                 onBackPressed();
             }
         });
+    }
+
+    protected void TUTAJFUNKCJA()
+    {
+        WSocket wSocket = WSocket.getwSocketInstance();
+        wSocket.sendData(UserRegistered.getAllUsers(UserLogged.getUserLoggedInstance().getToken()).toString());
+
+        List<UserRegistered> registeredUsers = new ArrayList<UserRegistered>();
+
+        synchronized (wSocket.notifier) {
+            try {
+                wSocket.notifier.wait();
+
+                JSONArray payload = wSocket.jsonMsg.getJSONArray("payload");
+                for (int i =0; i<payload.length();i++) {
+                    JSONObject userRegistered = payload.getJSONObject(i);
+                    UserRegistered registered = new UserRegistered(userRegistered.getString("username"),
+                            userRegistered.getString("email"),
+                            userRegistered.getString("id"));
+                    registeredUsers.add(registered);
+                }
+
+            } catch (Exception exep) {
+            }
+        }
     }
 }
