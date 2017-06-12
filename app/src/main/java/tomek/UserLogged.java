47fa8acdd1b8c933585fa312bca9fc0af2814dea
Tomek Demczuk <tomekkomek@gmail.com>
2017-06-12 14:47:21 +0200

    odratowane z niedzieli

--- app/src/main/java/tomek/UserLogged.java
+++ app/src/main/java/tomek/UserLogged.java
@@ -3,6 +3,7 @@ package tomek;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,6 +15,8 @@ public class UserLogged extends User implements JSONStringer {
     private String id;
     private String avatarid;
     private static UserLogged userLoggedInstance = null;
+    private List<UserRegistered> friends;
+
 
 
 
@@ -33,7 +36,140 @@ public class UserLogged extends User implements JSONStringer {
         super(email, userName);
         this.token = token;
         this.id = id;
+        friends = new ArrayList<UserRegistered>();
 
+    }
+
+
+
+    public void setFriends(ArrayList<UserRegistered> friendList)
+    {
+        friends=friendList;
+    }
+
+    public static JSONObject addFriend(String id, String token)
+    {
+        String scope = "friendship";
+        String method = "inviteFriend";
+
+        JSONObject procedureObject = new JSONObject();
+        JSONObject headerObject = new JSONObject();
+        JSONObject payloadObject = new JSONObject();
+        JSONObject finalObject = new JSONObject();
+
+        try {
+            headerObject.put("token", token);
+            procedureObject.put("scope", scope);
+            procedureObject.put("method", method);
+            payloadObject.put("id", id);
+            finalObject.put("procedure", procedureObject);
+            finalObject.put("meta", headerObject);
+            finalObject.put("payload", payloadObject);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return finalObject;
+
+    }
+
+    public static JSONObject removeFriend(String id, String token)
+    {
+
+        String scope = "friendship";
+        String method = "removeFriend";
+
+        JSONObject procedureObject = new JSONObject();
+        JSONObject headerObject = new JSONObject();
+        JSONObject payloadObject = new JSONObject();
+        JSONObject finalObject = new JSONObject();
+
+        try {
+            headerObject.put("token", token);
+            procedureObject.put("scope", scope);
+            procedureObject.put("method", method);
+            payloadObject.put("id", id);
+            finalObject.put("procedure", procedureObject);
+            finalObject.put("meta", headerObject);
+            finalObject.put("payload", payloadObject);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return finalObject;
+    }
+    public static JSONObject rejectFriend(String id, String token)
+    {
+
+        String scope = "friendship";
+        String method = "rejectFriendshipInvite";
+
+        JSONObject procedureObject = new JSONObject();
+        JSONObject headerObject = new JSONObject();
+        JSONObject payloadObject = new JSONObject();
+        JSONObject finalObject = new JSONObject();
+
+        try {
+            headerObject.put("token", token);
+            procedureObject.put("scope", scope);
+            procedureObject.put("method", method);
+            payloadObject.put("id", id);
+            finalObject.put("procedure", procedureObject);
+            finalObject.put("meta", headerObject);
+            finalObject.put("payload", payloadObject);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return finalObject;
+    }
+    public static JSONObject acceptFriend(String id, String token)
+    {
+        String scope = "friendship";
+        String method = "acceptFriendshipInvite";
+
+        JSONObject procedureObject = new JSONObject();
+        JSONObject headerObject = new JSONObject();
+        JSONObject payloadObject = new JSONObject();
+        JSONObject finalObject = new JSONObject();
+
+        try {
+            headerObject.put("token", token);
+            procedureObject.put("scope", scope);
+            procedureObject.put("method", method);
+            payloadObject.put("id", id);
+            finalObject.put("procedure", procedureObject);
+            finalObject.put("meta", headerObject);
+            finalObject.put("payload", payloadObject);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return finalObject;
+    }
+
+    public static JSONObject getFriends(String token)
+    {
+        String scope = "friendship";
+        String method = "getFriendsList";
+
+        JSONObject procedureObject = new JSONObject();
+        JSONObject headerObject = new JSONObject();
+        JSONObject payloadObject = new JSONObject();
+        JSONObject finalObject = new JSONObject();
+
+        try {
+            headerObject.put("token", token);
+            procedureObject.put("scope", scope);
+            procedureObject.put("method", method);
+            finalObject.put("procedure", procedureObject);
+            finalObject.put("meta", headerObject);
+            finalObject.put("payload", payloadObject);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return finalObject;
     }
 
 
