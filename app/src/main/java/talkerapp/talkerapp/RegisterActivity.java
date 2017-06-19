b394318d15d84d793039b63fddade3648c9dd826
Tomek Demczuk <tomekkomek@gmail.com>
2017-06-20 00:05:31 +0200

    Merge branch 'muttley' of https://github.com/MagnificentMuttley/TalkerApp into natsyu_PC
    # Conflicts:
    #	app/src/main/java/talkerapp/talkerapp/AccoutActivity.java
    #	app/src/main/java/talkerapp/talkerapp/RegisterActivity.java

--- app/src/main/java/talkerapp/talkerapp/RegisterActivity.java
+++ app/src/main/java/talkerapp/talkerapp/RegisterActivity.java
@@ -19,7 +19,6 @@ import javaClasses.UserAdd;
 import javaClasses.UserLogged;
 import javaClasses.WSocket;
 
-import static javaClasses.UserLogged.LoggedUserInfo;
 
 public class RegisterActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -127,7 +126,7 @@ public class RegisterActivity extends AppCompatActivity {
                         if (wSocket.status.equals("200"))
                         {
                             String token = wSocket.payload;
-                            wSocket.sendData(LoggedUserInfo(token).toString());
+                            wSocket.sendData(UserLogged.loggedUserInfo(token).toString());
     
                             synchronized (wSocket.notifier)
                             {
